d9b60a3bf32c4ac01a4f997c65a5619d8a6015ae
Хусаинова Ольга <altyn-bash@bars.group>
2024-06-15 16:16:07 +0300

    data_structure 1-4

--- block_1/data_structure/task_1/implementation.py
+++ block_1/data_structure/task_1/implementation.py
@@ -1,4 +1,9 @@
 class Tuple:
+
+    tpl = ()
+    def __init__(self, *args):
+        self.tpl = args
+
     """
     Создает неизменяемый объект с упорядоченной структурой и методами count и index.
     При создании принимается последовательность объектов.
@@ -11,7 +16,11 @@ class Tuple:
         Args:
             value: количество вхождений в объекте
         """
-        raise NotImplementedError
+        count = 0
+        for i in self.tpl:
+            if i == value:
+                count += 1
+        return count
 
     def index(self, value) -> int:
         """
@@ -20,4 +29,18 @@ class Tuple:
         Args:
             value: индекс искомого элемента
         """
-        raise NotImplementedError
+        idx = None
+        for i in range(0, len(self.tpl)):
+            if self.tpl[i] == value:
+                idx = i
+                break
+        if idx is None:
+            raise ValueError
+        else:
+            return idx
+    
+    def __getitem__(self, key):
+        if len(self.tpl) > 0 and key in range(0, len(self.tpl)):
+            return self.tpl[key]
+        else:
+            raise ValueError
--- block_1/data_structure/task_3/implementation.py
+++ block_1/data_structure/task_3/implementation.py
@@ -1,5 +1,14 @@
+import copy
+
 def copy_dict(origin_dict: dict) -> dict:
     """
     Функция возвращает копию словаря.
     """
+    new_dict = {}
+    for key, value in origin_dict.items():
+        if type(value) in [dict, list, tuple, set]:
+            new_dict.update({key: value.copy()})
+        else:
+            new_dict.update({key:value})
+    return new_dict
     raise NotImplementedError
