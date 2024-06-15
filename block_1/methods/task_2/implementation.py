2a8ba5ebe57940591aa80714412da25157c00a73
Хусаинова Ольга <altyn-bash@bars.group>
2024-06-15 11:50:46 +0300

    methods task2

--- block_1/methods/task_2/implementation.py
+++ block_1/methods/task_2/implementation.py
@@ -1,11 +1,27 @@
+from block_1.common import (
+    MyException,
+)
 class ClassFather:
-    pass
+    registered_list = []
+    _name = 'Class Father name'
 
+    @classmethod
+    def register(cls):
+        if issubclass(cls, ClassFather) and cls._name != 'Class Father name':
+            cls.registered_list.append(cls._name)
+        else:
+            raise MyException
+
+    @classmethod
+    def get_name(cls):
+        if issubclass(cls, ClassFather) and cls._name in cls.registered_list:
+            return cls._name
+        else:
+            raise MyException
 
 class User1(ClassFather):
-    _name = ''
+    _name = 'User1 name'
 
 
 class User2(ClassFather):
-    _name = ''
-
+    _name = 'User2 name'
