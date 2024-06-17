8421a87304a90a1aa952a4f183d00cb48d4a4460
Хусаинова Ольга <altyn-bash@bars.group>
2024-06-17 12:37:04 +0300

    generators 1

--- block_1/generators/task1/implementation.py
+++ block_1/generators/task1/implementation.py
@@ -1,2 +1,7 @@
-def fib():
-    raise NotImplementedError
+def fib(n):
+    first = next = 1
+    yield first
+    yield next
+    for i in range(2, n):
+        first, next = next, first + next
+        yield next
