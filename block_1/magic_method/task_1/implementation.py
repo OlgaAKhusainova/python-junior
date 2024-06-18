95e565704d5a35aad54d757a5ccabca0aeb06d16
Хусаинова Ольга <altyn-bash@bars.group>
2024-06-18 10:23:28 +0300

    magic_method 1, 2

--- block_1/magic_method/task_1/implementation.py
+++ block_1/magic_method/task_1/implementation.py
@@ -1,24 +1,55 @@
+from block_1.common import (
+    MyException,
+)
 class Multiplier:
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def __add__(self, obj):
+        if isinstance(obj, Multiplier):
+            return Multiplier(self.value + obj.value)
+        else:
+            raise MyException
+        
+    def __sub__(self, obj):
+        if isinstance(obj, Multiplier):
+            return Multiplier(self.value - obj.value)
+        else:
+            raise MyException
+    
+    def __mul__(self, obj):
+        if isinstance(obj, Multiplier):
+            return Multiplier(self.value * obj.value)
+        else:
+            raise MyException
+        
+    def __truediv__(self, obj):
+        if isinstance(obj, Multiplier):
+            return Multiplier(self.value / obj.value)
+        else:
+            raise MyException
+        
 
 
 class Hundred(Multiplier):
     """Множитель на 100"""
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.value = value * 100
 
 
 class Thousand(Multiplier):
     """Множитель на 1 000"""
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.value = value * 1000
 
 
 class Million(Multiplier):
     """Множитель на 1 000 000"""
-    def __init__(self):
-        pass
+    def __init__(self, value):
+        self.value = value * 1000000
 
 
 
--- block_1/magic_method/task_2/implementation.py
+++ block_1/magic_method/task_2/implementation.py
@@ -1,8 +1,46 @@
+from datetime import datetime, timedelta
+from block_1.common import (
+    MyException,
+)
+
 class MathClock:
     def __init__(self):
-        pass
-
+        self._time = datetime(2000, 1, 1, 0, 0)
+        
     def get_time(self):
-        return 0
+        return self._time.strftime("%H:%M")
+    
+    def __add__(self, min):
+        try:
+            result = self._time + timedelta(minutes=min)
+            self._time = result
+        except:
+            raise MyException
+        
+        return self._time
+        
+    def __mul__(self, hrs):
+        try:
+            result = self._time + timedelta(hours = hrs)
+            self._time = result
+        except:
+            raise MyException
+        return self._time
+    
+    def __sub__(self, min):
+        try:
+            result = self._time - timedelta(minutes= min)
+            self._time = result
+        except:
+            raise MyException
+        return self._time
+    
+    def __truediv__(self, hrs):
+        try:
+            result = self._time - timedelta(hours = hrs)
+            self._time = result
+        except:
+            raise MyException
+        return self._time
 
 
